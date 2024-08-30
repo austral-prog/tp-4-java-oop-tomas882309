b44578bdc1b7952793366425e1af03a2ee1e7dca
Tomas Scherer <tomassk2005@gmail.com>
2024-08-30 12:55:35 -0300

    tp4 java terminado

--- src/main/java/com/template/Elevator.java
+++ src/main/java/com/template/Elevator.java
@@ -1,6 +1,47 @@
 package com.template;
 
 public class Elevator {
+    int floor;
+    int passangers;
+    int minfloor;
+    int maxfloor;
+    int cantidadmaxima;
+
+    public Elevator(int minfloor, int maxfloor, int cantidadmaxima){
+        this.minfloor = minfloor;
+        this.maxfloor = maxfloor;
+        this.cantidadmaxima = cantidadmaxima;
+        passangers = 0;
+        floor = 0;
+    }
+    public int moveToFloor(int newfloor){
+        if (newfloor >= minfloor && newfloor <= maxfloor){
+            this.floor = newfloor;
+            return newfloor;
+        }
+        else{
+            return floor;
+        }
+    }
+    public int getCurrentFloor(){
+        return floor;
+    }
+    public int addPassenger(){
+        if (passangers < cantidadmaxima) {
+            this.passangers = passangers + 1;
+            return passangers;
+        }
+        else{
+            return passangers;
+        }
+    }
+    public int getPassengerCount(){
+        return passangers;
+    }
+    public int removePassenger(){
+        this.passangers = passangers - 1;
+        return passangers;
+    }
 
 
 
